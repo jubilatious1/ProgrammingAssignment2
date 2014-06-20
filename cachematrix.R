df717fdca39a1a858af13eb8456505c426811ac0
jubilatious1 <wjm1@caa.columbia.edu>
2014-06-20 15:02:26 -0700

    Rstudio_project_programming_assignment_2
    First cachematrix.R modification

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Caching the inverse of a Matrix
 
-## Write a short comment describing this function
+## Creates matrix and uses "<<-" operator to store a copy in cache.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+      get <- function() x
+      setmean <- function(mean) m <<- mean
+      getmean <- function() m
+      list(set = set, get = get,
+           setmean = setmean,
+           getmean = getmean)
 }
 
+makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
 
-## Write a short comment describing this function
+## Checks cache first before returning inverse matrix of x.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
